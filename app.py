7aabfe6a0353b7c9c18dd4b1d3e628ff9b1b83d3
Marcelo Veiga Neves <marcelo.veiga@gmail.com>
2022-11-08 21:14:59 -0300

    Add dynamodb insert after image upload

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ def upload_page():
             "date": int(time())
         }
         flash('{src} uploaded to S3 as {dst}'.format(src=form.example.data.filename, dst=output))
+        dynamodb_put_item(item)
     entries = dynamodb_scan()
     return render_template('example.html', form=form, entries=entries)
 
