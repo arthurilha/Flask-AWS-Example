25c85bda51013c2cc63bdc777ae8641da5ad36a2
Marcelo Veiga Neves <marcelo.veiga@gmail.com>
2022-11-10 21:20:38 -0300

    change aws access from credentials to role

--- dynamodb.py
+++ dynamodb.py
@@ -2,32 +2,15 @@ import boto3
 from flask import current_app as app
 
 def dynamodb_put_item(item_data):
-
-    session = boto3.Session(
-        aws_access_key_id=app.config["AWS_KEY"],
-        aws_secret_access_key=app.config["AWS_SECRET"],
-        aws_session_token=app.config["AWS_SESSION"]
-    )
-
+    session = boto3.Session(region_name="us-east-1")
     dynamodb = session.resource("dynamodb")
-
     table = dynamodb.Table(app.config["DYNAMODB_TABLE"])
-
     return table.put_item(Item=item_data)
 
 
 def dynamodb_scan():
-
-    session = boto3.Session(
-        aws_access_key_id=app.config["AWS_KEY"],
-        aws_secret_access_key=app.config["AWS_SECRET"],
-        aws_session_token=app.config["AWS_SESSION"]
-	)
-
+    session = boto3.Session(region_name="us-east-1")
     dynamodb = session.resource("dynamodb")
-
     table = dynamodb.Table(app.config["DYNAMODB_TABLE"])
-
     response = table.scan()
-
     return response.get('Items', [])
