bdcb2991bcc56157b12b76d3da0128e0dc22ccc7
Arthur Battistel Ilha <73798535+arthurilha@users.noreply.github.com>
2023-06-26 20:28:00 -0300

    Update dynamodb.py

--- dynamodb.py
+++ dynamodb.py
@@ -4,13 +4,13 @@ from flask import current_app as app
 def dynamodb_put_item(item_data):
     session = boto3.Session(region_name="us-east-1")
     dynamodb = session.resource("dynamodb")
-    table = dynamodb.Table(app.config["DYNAMODB_TABLE"])
+    table = dynamodb.Table("trab-final-dynamo")
     return table.put_item(Item=item_data)
 
 
 def dynamodb_scan():
     session = boto3.Session(region_name="us-east-1")
     dynamodb = session.resource("dynamodb")
-    table = dynamodb.Table(app.config["DYNAMODB_TABLE"])
+    table = dynamodb.Table("trab-final-dynamo")
     response = table.scan()
     return response.get('Items', [])
