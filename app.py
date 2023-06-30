c03180513b4079addc4008e23a7712cc3752973e
Arthur Battistel Ilha <73798535+arthurilha@users.noreply.github.com>
2023-06-29 21:32:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 app.config.from_object("config")
 
 class UploadForm(FlaskForm):
-    example = FileField("Example Files")
+    example = FileField("Example Files aqui")
 
 @app.route("/", methods=["POST", "GET"])
 def upload_page():
